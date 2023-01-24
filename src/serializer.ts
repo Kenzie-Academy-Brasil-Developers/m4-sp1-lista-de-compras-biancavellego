f217024331521b8312a455d5c42e46579f4da226
Bianca Vellego <biancavell@hotmail.com>
2023-01-24 12:10:01 -0300

    feat: added more restrictions to all serializers, not allowing client to insert wrong data types as keys + exceed the key limit.

--- src/serializer.ts
+++ src/serializer.ts
@@ -10,6 +10,16 @@ const validateListData = (payload: any): IList => {
   const payloadKeys: string[] = Object.keys(payload);
   const requiredKeys: listRequiredFields[] = ["listName", "data"];
 
+  if (requiredKeys.length > 2) {
+    response.status(400).json({ message: "Exceeded maximum number of keys" });
+  }
+
+  if (typeof requiredKeys[0] !== "string") {
+    response
+      .status(400)
+      .json({ message: "The list name needs to be a string" });
+  }
+
   const hasRequiredKeys: boolean = requiredKeys.every((key: string) =>
     payloadKeys.includes(key)
   );
@@ -30,6 +40,19 @@ const validateProductData = (payload: any): IProduct => {
   const payloadKeys: string[] = Object.keys(payload);
   const requiredKeys: productRequiredFields[] = ["name", "quantity"];
 
+  if (requiredKeys.length > 2) {
+    response.status(400).json({ message: "Exceeded maximum number of keys" });
+  }
+
+  if (
+    typeof requiredKeys[0] !== "string" ||
+    typeof requiredKeys[1] !== "string"
+  ) {
+    response.status(400).json({
+      message: "The product's field names data type need to be string",
+    });
+  }
+
   const hasRequiredKeys: boolean = requiredKeys.every((key: string) =>
     payloadKeys.includes(key)
   );
@@ -47,7 +70,3 @@ const validateProductData = (payload: any): IProduct => {
 };
 
 export { validateListData, validateProductData };
-
-// {
-//     "message": "The list name need to be a string"
-// }
