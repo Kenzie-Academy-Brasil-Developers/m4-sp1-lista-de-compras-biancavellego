c004afd77bfec80880e5fa229362097fdb8fb06a
Bianca Vellego <biancavell@hotmail.com>
2023-01-24 11:58:46 -0300

    feat: created serializers to validate list fields + product fields.

--- src/serializer.ts
+++ src/serializer.ts
@@ -1,3 +1,4 @@
+import { response } from "express";
 import {
   IList,
   IProduct,
@@ -5,15 +6,47 @@ import {
   listRequiredFields,
 } from "./interfaces";
 
-const validateListData = (payload: any): IList => {};
+const validateListData = (payload: any): IList => {
+  const payloadKeys: string[] = Object.keys(payload);
+  const requiredKeys: listRequiredFields[] = ["listName", "data"];
 
-const validateProductData = (payload: any): IProduct => {};
+  const hasRequiredKeys: boolean = requiredKeys.every((key: string) =>
+    payloadKeys.includes(key)
+  );
+
+  if (hasRequiredKeys === false) {
+    const key1: string = requiredKeys[0];
+    const key2: string = requiredKeys[1];
+
+    response
+      .status(400)
+      .json({ message: `Updatable fields are: ${key1} and ${key2}` });
+  }
+
+  return payload;
+};
+
+const validateProductData = (payload: any): IProduct => {
+  const payloadKeys: string[] = Object.keys(payload);
+  const requiredKeys: productRequiredFields[] = ["name", "quantity"];
+
+  const hasRequiredKeys: boolean = requiredKeys.every((key: string) =>
+    payloadKeys.includes(key)
+  );
+
+  if (hasRequiredKeys === false) {
+    const key1: string = requiredKeys[0];
+    const key2: string = requiredKeys[1];
+
+    response
+      .status(400)
+      .json({ message: `Updatable fields are: ${key1} and ${key2}` });
+  }
+
+  return payload;
+};
 
 export { validateListData, validateProductData };
-
-// {
-//     "message": "Updatable fields are: \"name\" and \"quantity\""
-// }
 
 // {
 //     "message": "The list name need to be a string"
